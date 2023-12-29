57d3ecb4e0f3035664f121e43753dc81e27c0339
Ivan Satsiuk <sativ01.ca@gmail.com>
2023-12-29 12:07:34 +0100

    move to bun/vite

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { Typography } from '@mui/material';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import * as React from 'react';
 import { getDates } from './api/data';
 
 export default function App() {
---
+++ frontend/src/main.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+
+import App from './App.tsx';
+import theme from './theme';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <App />
+    </ThemeProvider>,
+  </React.StrictMode>,
+)

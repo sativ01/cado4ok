7829157b3adef32dd4dd45e6a3ecf4da2149c5d9
Ivan Satsiuk <sativ01.ca@gmail.com>
2023-12-29 11:45:28 +0100

    be sources

---
+++ backend/prisma/seed.ts
@@ -0,0 +1,33 @@
+import { PrismaClient, Role } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const userData = [
+  {
+    name: 'Admin',
+    email: 'admin@prisma.io',
+    phone: '2435234',
+    role: Role.ADMIN,
+  },
+]
+
+async function main() {
+  console.log(`Start seeding ...`)
+  for (const u of userData) {
+    const user = await prisma.user.create({
+      data: u,
+    })
+    console.log(`Created user with id: ${user.id}`)
+  }
+  console.log(`Seeding finished.`)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
